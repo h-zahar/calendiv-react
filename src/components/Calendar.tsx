28f28bd165a0fe4929aee95c22475a129ab86ad7
h-zahar <hz.rifat@gmail.com>
2024-01-09 10:50:08 +0600

    Implemented Dynamic Date Display

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,8 +1,40 @@
+import {
+  addDays,
+  endOfMonth,
+  getDate,
+  getDay,
+  getDaysInMonth,
+  startOfMonth,
+  subDays,
+} from "date-fns";
+
 const Calendar = () => {
+  // const test = Array(35)
+  //   .fill(1)
+  //   .map((t, i) => t * i + 1);
+
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const test = Array(35)
+  const dates = Array(getDaysInMonth(new Date()))
     .fill(1)
     .map((t, i) => t * i + 1);
+
+  const numOfPrevDays = Array(getDay(startOfMonth(new Date()))).fill(1);
+  const prevDays = numOfPrevDays.map((d, i) =>
+    getDate(subDays(startOfMonth(new Date()), numOfPrevDays.length - i))
+  );
+  dates.unshift(...prevDays);
+
+  // if (arr.length < 35) {
+  const numOfNextDays = Array(6 - getDay(endOfMonth(new Date()))).fill(1);
+  const nextDays = numOfNextDays.map((d, i) =>
+    getDate(addDays(startOfMonth(new Date()), d * i))
+  );
+  // }
+  dates.push(...nextDays);
+
+  // console.log(getDay(startOfMonth(new Date())));
+  // console.log(getDate(subDays(startOfMonth(new Date()), 5)));
+  // console.log(startOfMonth(new Date()));
 
   return (
     <div
@@ -15,7 +47,7 @@ const Calendar = () => {
         margin: "60px 30px auto 30px",
       }}
     >
-      {test.map((t, i) => {
+      {dates.map((t, i) => {
         return (
           <div
             style={{
--- src/components/MonthlyView.tsx
+++ src/components/MonthlyView.tsx
@@ -1,10 +1,40 @@
+import {
+  addDays,
+  endOfMonth,
+  getDate,
+  getDay,
+  getDaysInMonth,
+  startOfMonth,
+  subDays,
+} from "date-fns";
+
 const MonthlyView = () => {
-  const test = Array(35)
+  // const test = Array(35)
+  //   .fill(1)
+  //   .map((t, i) => t * i + 1);
+
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dates = Array(getDaysInMonth(new Date()))
     .fill(1)
     .map((t, i) => t * i + 1);
 
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const numOfPrevDays = Array(getDay(startOfMonth(new Date()))).fill(1);
+  const prevDays = numOfPrevDays.map((d, i) =>
+    getDate(subDays(startOfMonth(new Date()), numOfPrevDays.length - i))
+  );
+  dates.unshift(...prevDays);
 
+  // if (arr.length < 35) {
+  const numOfNextDays = Array(6 - getDay(endOfMonth(new Date()))).fill(1);
+  const nextDays = numOfNextDays.map((d, i) =>
+    getDate(addDays(startOfMonth(new Date()), d * i))
+  );
+  // }
+  dates.push(...nextDays);
+
+  // console.log(getDay(startOfMonth(new Date())));
+  // console.log(getDate(subDays(startOfMonth(new Date()), 5)));
+  // console.log(startOfMonth(new Date()));
   return (
     <div
       style={{
@@ -19,7 +49,7 @@ const MonthlyView = () => {
         border: "0.5px solid #dadce0",
       }}
     >
-      {test.map((t, i) => {
+      {dates.map((t, i) => {
         return (
           <div
             style={{
